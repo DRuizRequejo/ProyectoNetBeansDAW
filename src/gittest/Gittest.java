512ef12b1c269638eb090c105cf0c573de0c68fb
DRuizRequejo <druizrequejo@gmail.com>
2017-11-26 19:08:31 +0100

    Cambio Selección equipo

--- src/gittest/Gittest.java
+++ src/gittest/Gittest.java
@@ -15,15 +15,39 @@ public class Gittest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int num;
-        int cont = 0;
-        while (cont < 15) {            
-            num = (int) (Math.random() * 10)+1;
-            if(num < 5){
-                System.out.println("Hello");
-            }
-            cont++;
+        int num = (int) (Math.random() * 10) + 1;
+        switch (num) {
+            case 1:
+                System.out.println("Mi equipo es el Zaragoza");
+                break;
+            case 2:
+                System.out.println("Mi equipo es el Huesca");
+                break;
+            case 3:
+                System.out.println("Mi equipo es el Real Madrid");
+                break;
+            case 4:
+                System.out.println("Mi equipo es el FCB Barcelona");
+                break;
+            case 5:
+                System.out.println("Mi equipo es el Atlético de Madrid");
+                break;
+            case 6:
+                System.out.println("Mi equipo es el Inazuma Eleven");
+                break;
+            case 7:
+                System.out.println("Mi equipo es el New Team");
+                break;
+            case 8:
+                System.out.println("Mi equipo es la Juventus");
+                break;
+            case 9:
+                System.out.println("Mi equipo es el Galaxy");
+                break;
+            case 10:
+                System.out.println("Mi equipo es la Selección Española");
+                break;
         }
     }
-
 }
+
